bea1cab29e31e1432b8932ec1e82bf2169b34d51
Erik <emauring@hotmail.com>
2014-12-02 22:04:22 +0100

    c'est fin

--- Code/main.py
+++ Code/main.py
@@ -1,26 +1,25 @@
 import feedparser
 import urllib
 import os
-import re
-#import logging
 
-def rssChecker(s):
-	rssLen = len(s.entries)
-	podLink = s.entries[0].link
-	podName = s.entries[0].title
+def rssInfoRunner(stream):
+	rssLen = len(stream.entries)
+	podLink = stream.entries[0].link
+	podName = stream.entries[0].title
 
-	stuff = []
-	folder = os.walk(".")
+	return podLink, podName
+
+def Checker(fu):
+	folder = os.walk("E:\Git Prosjekter\RSSThingy\Code")
 
 	for ep in folder:
-		#goget = re.search(r'main',ep)
-		print ep
+		epList = ep[2]
 
+	for thing in epList:	
+		if thing.find(fu) == 0:
+			return True
 
-	print stuff
-
-	return (podLink,podName)
-
+	return False
 
 def downloader(podcastUrl,filename):
 	fileRetriever = urllib.URLopener()
@@ -28,22 +27,11 @@ def downloader(podcastUrl,filename):
 		fileRetriever.retrieve(podcastUrl, filename + '.mp3')
 	except Exception, e:
 		raise e
-		#Could throw in some logging here..
 
 if __name__ == '__main__':
 	#while True:
 		podStream = feedparser.parse('http://www.p4.no/lyttesenter/podcast.ashx?pid=330')
-
-		rssChecker(podStream)
-		#downloader(rssChecker(podStream)[0],rssChecker(podStream)[1])
+		if Checker(rssInfoRunner(podStream)[1]) == False:
+			downloader(rssInfoRunner(podStream)[0],rssInfoRunner(podStream)[1])
+			print 'Downloading ' + rssInfoRunner(podStream)[1]
 		#time.sleep(86400) #Sleep for 24 hours. 
-
-
-
-#Need class for downloading and class for reading. Reading class is opening feed, identifying if there are new podcasts.
-
-
-#Downloader class is obviously for downloading. Furthermore, it need to name the podcasts accordingly. Method instead?
-#The name shall be derived from entries[i].title
-
-#Problems: Determining if podcasts have been downloaded already. Could read files in download folder, but naming..
